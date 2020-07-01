e53921330290cd7ad7f90ef2f9f4a44786b549ba
SayusiAndo <sayusi.ando@gmail.com>
2020-07-01 20:22:26 +0200

    #8: - BcButton enabled/disabled state tests

--- BlazoredCarbon.Test/Components/Button/ButtonComponent_EnabledDisabled_Should.cs
+++ BlazoredCarbon.Test/Components/Button/ButtonComponent_EnabledDisabled_Should.cs
@@ -1,23 +1,48 @@
 namespace SayusiAndo.BlazoredCarbon.Test.Components.Button
 {
+    using System;
     using System.Threading.Tasks;
+    using AngleSharp.Dom;
+    using Bunit;
+    using Carbon.BlazoredCarbon.Components.Button;
+    using FluentAssertions;
     using Xunit;
 
-    public class ButtonComponent_EnabledDisabled_Should
+    public class ButtonComponent_EnabledDisabled_Should : TestContext
     {
         [Fact]
         public async Task BeEnabled_ByDefault()
         {
+            // Arrange
+            IRenderedComponent<BcButton> cut = RenderComponent<BcButton>();
+
+            // Assert
+            cut.Find("button").ToMarkup().Contains("enabled").Should().BeFalse();
+            cut.Find("button").ToMarkup().Contains("disabled").Should().BeFalse();
         }
 
         [Fact]
         public async Task BeEnabled_WhenEnabledStateIsConfigured()
         {
+            // Arrange
+            IRenderedComponent<BcButton> cut = RenderComponent<BcButton>(
+                ("State", State.Enabled));
+
+            // Assert
+            cut.Find("button").ToMarkup().Contains("enabled").Should().BeFalse();
+            cut.Find("button").ToMarkup().Contains("disabled").Should().BeFalse();
         }
 
         [Fact]
         public async Task BeDisabled_WhenDisabledStateIsConfigured()
         {
+            // Arrange
+            IRenderedComponent<BcButton> cut = RenderComponent<BcButton>(
+                ("State", State.Disabled));
+
+            // Assert
+            cut.Find("button").ToMarkup().Contains("enabled").Should().BeFalse();
+            cut.Find("button").ToMarkup().Contains("disabled").Should().BeTrue();
         }
     }
 }
