b6f6ac74984ebf82c731496a9a7a7eac172a85b1
SayusiAndo <sayusi.ando@gmail.com>
2020-07-04 12:39:19 +0200

    - missing files

---
+++ BlazoredCarbon.Test/Components/Button/ButtonApi.cs
@@ -0,0 +1,10 @@
+namespace SayusiAndo.BlazoredCarbon.Test.Components.Button
+{
+    public static class ButtonApi
+    {
+        public const string Type = "type";
+        public const string Kind = "kind";
+        public const string Size = "size";
+        public const string IsDisabled = "isdisabled";
+    }
+}
